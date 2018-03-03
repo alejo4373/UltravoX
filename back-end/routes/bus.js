6dbd2e0a8615e4a07a905c691db273b07b7b843a
Alejandro F <alejo4373@gmail.com>
2018-03-03 14:10:13 -0500

    add endpoint to query info for a specific route i.e QM1

---
+++ back-end/routes/bus.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+var axios = require('axios')
+
+router.get('/:busroute', function(req, res, next) {
+  axios
+    .get(`http://bustime.mta.info/api/search?q=${req.params.busroute}`)
+    .then(data => {
+      res.status(200).json(data.data)
+      console.log(data.data)
+    }) 
+    .catch(err => next(err))
+});
+
+
+module.exports = router;
