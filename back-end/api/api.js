f9a82c5aa35ab418d0815c8564ce9da4183a4473
Alejandro F <alejo4373@gmail.com>
2018-03-03 22:13:24 -0500

    add api interface

---
+++ back-end/api/api.js
@@ -0,0 +1,41 @@
+var axios = require('axios')
+var apiKey = 'b6ce6ad1-765b-4084-b947-02d9cdeb0610';
+
+const getEta = (busId, stopId, callback) => {
+  axios
+    .get(`http://bustime.mta.info/api/siri/stop-monitoring.json?key=${apiKey}&OperatorRef=MTA&MonitoringRef=${stopId}&LineRef=${busId}&version=2`)
+    .then(apiRes => {
+      // console.log(apiRes.data)
+      // console.log('ETA', apiRes.data.Siri.ServiceDelivery.StopMonitoringDelivery[0].MonitoredStopVisit[0].MonitoredVehicleJourney.MonitoredCall.ExpectedArrivalTime)
+      // console.log('ETA', apiRes.data.Siri.ServiceDelivery.StopMonitoringDelivery[0].MonitoredStopVisit[0].MonitoredVehicleJourney.MonitoredCall)
+      var resObj = {
+        eta: apiRes.data.Siri.ServiceDelivery.StopMonitoringDelivery[0].MonitoredStopVisit[0].MonitoredVehicleJourney.MonitoredCall.ExpectedArrivalTime,
+        proximityText: apiRes.data.Siri.ServiceDelivery.StopMonitoringDelivery[0].MonitoredStopVisit[0].MonitoredVehicleJourney.MonitoredCall.ArrivalProximityText
+      }
+      callback(resObj)
+    })
+    .catch(err => {
+      throw err
+     // next(err)
+    })
+}
+
+const getBusIdAndFireGetEta = (busName, callback) => {
+  axios
+    .get(`http://bustime.mta.info/api/search?q=${busName}`)
+    .then(data => {
+        var busId = data.data.searchResults.matches[0].id
+        var stopId = '550669'
+        getEta(busId, stopId, callback)
+    })
+     .catch(err => {
+       throw err
+       //next(err)
+      })
+}
+
+module.exports ={
+   getBusIdAndFireGetEta, 
+}
+
+getBusIdAndFireGetEta('q49', console.log)
--- back-end/routes/bus.js
+++ back-end/routes/bus.js
@@ -2,21 +2,11 @@ var express = require('express');
 var router = express.Router();
 var axios = require('axios')
 
-router.get('/closest', function (req, res, next) {
+router.post('/get_time', function (req, res, next) {
+  var bus = 
+  var stopId = req.body.stop_id;
   axios
-    .get(`http://bustime.mta.info/api/where/stops-for-location.json?lat=40.758512&lon=-73.881175&latSpan=0.005&lonSpan=0.005&key=6df76799-8845-447c-b2c9-e3fdd328ced2`)
-    .then(data => {
-      console.log(data)
-      res
-        .status(200)
-        .json(data.data)
-    })
-    .catch(err => next(err))
-});
-
-router.get('/:busroute', function (req, res, next) {
-  axios
-    .get(`http://bustime.mta.info/api/search?q=${req.params.busroute}`)
+    .get(`http://bustime.mta.info/api/siri/stop-monitoring.json?key=b6ce6ad1-765b-4084-b947-02d9cdeb0610&OperatorRef=MTA&MonitoringRef=550669&DirectionRef=0&LineRef=MTABC_Q49&version=2)
     .then(data => {
       res
         .status(200)
